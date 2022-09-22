92468f98ed5fc2c2cdf51bd6614fa81caec54cb4
Andrey Vishcenco <Andropoles@bk.ru>
2022-09-23 00:30:40 +0400

    12

--- C#/1/Program.cs
+++ C#/1/Program.cs
@@ -1,8 +1,62 @@
-﻿Console.WriteLine("Введите имя");
-string username = Console.ReadLine();
-Console.Write("Привет,ЗАЛУПА ");
-Console.Write(username);
-Console.WriteLine();
+﻿// Console.WriteLine("Введите имя");
+// string username = Console.ReadLine();
+// Console.Write("Привет,ЗАЛУПА ");
+// Console.Write(username);
+// Console.WriteLine();
+
+// int numberA = 3;
+// int numberB = 1500000;
+// int r = numberA + numberB;
+// Console.WriteLine(r);
+
+
+
+// double numberA = 1200000;
+// double numberB = 1500000;
+// double r = numberA * numberB;
+// Console.WriteLine(r);
+
+// new Random().Next(1,10)        // случайные числа от 1 до 10
+
+// double numberA = new Random().Next(1,10);  // случайные числа от 1 до 10
+// Console.WriteLine(numberA);
+// double numberB = new Random().Next(10,15);
+// Console.WriteLine(numberB);
+// double r = numberA - numberB;
+// Console.WriteLine(r);
+
+
+// Console.WriteLine("Введите имя пользователя");
+// string username = Console.ReadLine();
+
+// if(username.ToLower() == "андрей")
+// {
+//     Console.WriteLine("Привет БРАТАН");
+// }
+// else
+// {
+//     Console.WriteLine("Соси пипирку !");
+// }
+
+
+// int a = 1;
+// int b = 15;
+// int c = 5;
+// int d = 7;
+// int i = 111;
+// int w = 12;
+
+// int max = a;
+
+// if ( a > max ) max = a;
+// if ( b > max ) max = b;
+// if ( c > max ) max = c;
+// if ( d > max ) max = d;
+// if ( i > max ) max = i;
+// if ( w > max ) max = w;
+// Console.Write("Max = ");
+// Console.WriteLine(max);
+
 
 
 
