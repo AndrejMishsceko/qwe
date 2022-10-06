544befb76aa65f252aff5da5749e2b7b9a2dd986
Andrey Vishcenco <Andropoles@bk.ru>
2022-10-06 16:46:33 +0400

    1

--- Python/hi p/hi python.py
+++ Python/hi p/hi python.py
@@ -1,8 +1,39 @@
-print ("helloy залупа")
+# print ("helloy залупа")
 
-g = 12
-f = 2
-print(f + g)
-g = 100
-f = 2
-print(f + g)
+# g = 12
+# f = 2
+# print(f + g)
+# g = 100
+# f = 2
+# print(f + g)
+
+
+# задача первое число является ли квадратом первого
+
+# from multiprocessing.dummy import Array
+
+
+# a = int(input('Введите первое значение '))
+# b = int(input('Введите второе значение '))
+
+# if a== b*b or b==a*a:
+#     print('да')
+# else:
+#     print('нет')
+
+
+# Array=[]
+# for i in range(5):
+#     z = int(input("Введите число"))
+#     Array.append(z)
+
+# print(Array)
+
+# num = 0
+# for i in Array:
+#     if Array[i]>num:
+#         num=i
+# print(num)
+
+
+
