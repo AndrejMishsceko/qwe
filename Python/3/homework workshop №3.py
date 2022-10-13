d7cd9881936e755ce1e8931e4056c178aa62a5fe
Andrey Vishcenco <Andropoles@bk.ru>
2022-10-13 22:57:57 +0400

    6 домашнее задание

--- Python/3/homework workshop №3.py
+++ Python/3/homework workshop №3.py
@@ -98,35 +98,35 @@
 # ###############################             Решение
 
 
-def fibonacci(n):
-    first, second = 0, 1
-    fibonacci_num = 0
-    for i in range(n):
-        fibonacci_num = first + second
-        second = first
-        first = fibonacci_num
-    return fibonacci_num
+# def fibonacci(n):
+#     first, second = 0, 1
+#     fibonacci_num = 0
+#     for i in range(n):
+#         fibonacci_num = first + second
+#         second = first
+#         first = fibonacci_num
+#     return fibonacci_num
 
-def negative_fibonacci(n):
-    if n ==1:
-        return 1
-    elif n == 2:
-        return -1
-    else:
-        num1, num2 = 1, -1
-        for i in range(2, n):
-            num1, num2 = num2, num1-num2
-        return num2
+# def negative_fibonacci(n):
+#     if n ==1:
+#         return 1
+#     elif n == 2:
+#         return -1
+#     else:
+#         num1, num2 = 1, -1
+#         for i in range(2, n):
+#             num1, num2 = num2, num1-num2
+#         return num2
 
-set_number = (input("Ввудите число: "))
-while not set_number.isdigit():
-    set_number = (input("Введите число "))
-set_number = int(set_number)
-list = [0]
-for i in range(1,set_number +1):
-    list.append(fibonacci(i))
-    list.insert(0,negative_fibonacci(i))
-print(list)
+# set_number = (input("Ввудите число: "))
+# while not set_number.isdigit():
+#     set_number = (input("Введите число "))
+# set_number = int(set_number)
+# list = [0]
+# for i in range(1,set_number +1):
+#     list.append(fibonacci(i))
+#     list.insert(0,negative_fibonacci(i))
+# print(list)
 
 
 
--- Python/5/homework workshop №5.py
+++ Python/5/homework workshop №5.py
@@ -735,56 +735,67 @@
 ###############                             Решение №2
 
 
-def encoding(text):
-    code_text = ""
-    count = 0
-    repetitions = 1
-    while count < len(text):
-        try:
-            if text[count] == text[count+1]:
-                repetitions += 1
-            elif repetitions == 1:
-                code_text += text[count]
-            elif repetitions > 1:
-                code_text += str(repetitions) + text[count]
-                repetitions = 1
-        except IndexError:
-            if repetitions == 1:
-                code_text += text[count]
-            elif repetitions > 1:
-                code_text += str(repetitions) + text[count]
-        count += 1
-    return code_text
-# Function to decode given cipher
-def decoding(cipher):
-    decoded_text = ""
-    count = 0
-    repetitions = 0
-    while count < len(cipher):
-        if str(cipher[count]).isdigit():
-            repetitions = int(cipher[count])
-        elif repetitions > 0:
-            for x in range(repetitions):
-                decoded_text += cipher[count]
-                repetitions = 0
-        elif repetitions == 0:
-            decoded_text += cipher[count]
-        count +=1
-    return decoded_text
-# Text input  
-text = input("Enter a text: ")
-# Decides encode or decode
-numbers_in_text = 0
-for num in text:
-    if num.isdigit():
-        numbers_in_text += 1
-# If any digits exists it mean coded text, decoding
-if numbers_in_text > 0:
-    print(f"Decoding: {decoding(text)}")
-# If no digits exists it mean pure text, encoding
-else:
-    print(f"Encoding: {encoding(text)}")
+# def encoding(text):
+#     code_text = ""
+#     count = 0
+#     repetitions = 1
+#     while count < len(text):
+#         try:
+#             if text[count] == text[count+1]:
+#                 repetitions += 1
+#             elif repetitions == 1:
+#                 code_text += text[count]
+#             elif repetitions > 1:
+#                 code_text += str(repetitions) + text[count]
+#                 repetitions = 1
+#         except IndexError:
+#             if repetitions == 1:
+#                 code_text += text[count]
+#             elif repetitions > 1:
+#                 code_text += str(repetitions) + text[count]
+#         count += 1
+#     return code_text
+# # Function to decode given cipher
+# def decoding(cipher):
+#     decoded_text = ""
+#     count = 0
+#     repetitions = 0
+#     while count < len(cipher):
+#         if str(cipher[count]).isdigit():
+#             repetitions = int(cipher[count])
+#         elif repetitions > 0:
+#             for x in range(repetitions):
+#                 decoded_text += cipher[count]
+#                 repetitions = 0
+#         elif repetitions == 0:
+#             decoded_text += cipher[count]
+#         count +=1
+#     return decoded_text
+# # Text input  
+# text = input("Enter a text: ")
+# # Decides encode or decode
+# numbers_in_text = 0
+# for num in text:
+#     if num.isdigit():
+#         numbers_in_text += 1
+# # If any digits exists it mean coded text, decoding
+# if numbers_in_text > 0:
+#     print(f"Decoding: {decoding(text)}")
+# # If no digits exists it mean pure text, encoding
+# else:
+#     print(f"Encoding: {encoding(text)}")
+
+###########                                     Кодировка и раскодировка из файлов
 
 
-
-
+# with open('decode.txt', 'r') as data: # считали исходный текст
+#     my_text = data.read()
+# print('Исходный текст', my_text)
+# code_text = encode_file(my_text) # закодировали исходный текст
+# print('Текст после кодирования', code_text)
+# with open('encode.txt', 'w') as data: # записали закодированный текст
+#     data.write(code_text)
+# with open('encode.txt', 'r') as data: # считали закодированый текст
+#     str_code = data.read()
+# my_text = decode_file(str_code) # раскодировали текст
+# print('Раскодированный текст', my_text)
